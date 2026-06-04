002401b4ee96afacec46b1e3b8a093f6b4988305
Djibril-smb <abdoulaye-djibril.diakite@etu.univ-smb.fr>
2026-06-04 11:17:57 +0200

    creation des classes recettes et categorie

--- Paul/Paul/obj/Debug/net8.0-windows/Paul.AssemblyInfo.cs
+++ Paul/Paul/obj/Debug/net8.0-windows/Paul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Paul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d83834fb997810c1eb1eeb3499b1dc3606370f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119a764f4311014420f675789caa31e0e71d2e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Paul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Paul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
